1844ea1ca61d92060f43e4ba0b95dc1a38717d74
José Elias <108593215+joseegc@users.noreply.github.com>
2023-09-11 17:51:51 -0300

    Add files via upload

---
+++ Aula0409/Aula0409/Aula0409App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula0409App.swift
+//  Aula0409
+//
+//  Created by Student10 on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula0409App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
