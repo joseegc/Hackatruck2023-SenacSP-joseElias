a6d18807dec4da3c3caf4e73571b6b23365609f3
José Elias <108593215+joseegc@users.noreply.github.com>
2023-09-11 17:52:41 -0300

    Add files via upload

---
+++ Aula0409/Aula0409-2/View1.swift
@@ -0,0 +1,25 @@
+//
+//  View1.swift
+//  Aula0409-2
+//
+//  Created by Student10 on 04/09/23.
+//
+
+import SwiftUI
+
+struct View1: View {
+    var body: some View {
+        Text("Edmilson")
+        Text("edmilson@gmail.com")
+        Text("Telefone: (11) 99999-9999")
+
+        
+
+    }
+}
+
+struct View1_Previews: PreviewProvider {
+    static var previews: some View {
+        View1()
+    }
+}
