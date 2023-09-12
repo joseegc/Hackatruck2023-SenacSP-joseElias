567e4e898cf3a740e529f016e8d7fe928e0fef3b
José Elias <108593215+joseegc@users.noreply.github.com>
2023-09-12 18:02:00 -0300

    SwiftUI - Estilização de App de Música
    Desenvolvimento de aplicação para simular um aplicativo de música, implementação de elementos como Scroll View, For Each, dentre outros

---
+++ Aula0509/Aula0509/Aula0509App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula0509App.swift
+//  Aula0509
+//
+//  Created by Student10 on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula0509App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
