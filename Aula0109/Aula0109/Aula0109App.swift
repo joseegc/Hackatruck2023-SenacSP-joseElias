bb3ada00309657216831865e651228031ba29960
José Elias <108593215+joseegc@users.noreply.github.com>
2023-09-12 14:33:21 -0300

    Calculadora de IMC com SwiftUI
    Aplicação para Calcular um IMC de acordo com a entrada de dados do usuário (peso e altura), ao realizar o cálculo o design da página é alterado, como, por exemplo, alterando a cor de fundo

---
+++ Aula0109/Aula0109/Aula0109App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula0109App.swift
+//  Aula0109
+//
+//  Created by Student10 on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula0109App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
